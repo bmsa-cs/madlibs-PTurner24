a7fd4cd790bbf56427fec7df84cff5e64860b23c
Patricia Turner <pturner24@biomedscienceacademy.org>
2020-12-16 21:41:24 +0000

    I finished the code.

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Patricia Turner
+Period/Core: 2
 """
+
+print("Let's play Silly Sentences!")
+
+print()
+
+name = input("Enter a name: ")
+warm_drink = input("Enter a warm drink: ")
+adjective = input("Enter a adjective: ")
+color = input("Enter a color: ")
+color2 = input("Enter a differnt color: ")
+number = input("Enter number: ")
+animel = input("Enter animel: ")
+name2 = input("Enter a differnt name: ")
+feeling = input("Enter a feeling: ")
+
+print()
+
+print("It was like a blizzard outside with all this snow falling to the ground. So " +name+ " decided to make " +warm_drink+ " to warm themselves up.\n")
+
+print(f"It was also close to the holidays and the anticipation was  {adjective}. The house was decorated in a {color} and {color2} theme. \nIt had taken them {number} hours to get it just right to their liking.\n")
+
+print(f"There was even a room decorated completely for their {animel}, {name2}. \nAlthough {name} might have gone overboard, who cares as long as they were {feeling}.")
